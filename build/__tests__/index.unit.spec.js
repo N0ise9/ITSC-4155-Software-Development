675c36cee7d82b8ba98ad944e0b9f23174bf182b
JacobHarland <geicojacob@gmail.com>
2023-03-08 17:54:50 -0500

    remove build files

--- build/__tests__/index.unit.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const __1 = require("..");
-// This file will be deleted in the future
-// It is only here for github workflow success
-it("runs", () => {
-    const msg = (0, __1.test)();
-    expect(msg).toBe("test");
-});
--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = void 0;
-/* eslint-disable no-console */
-function test() {
-    return "test";
-}
-exports.test = test;
