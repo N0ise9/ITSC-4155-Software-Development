8c2d77f0e0605ecaa2cddcd3f72150aa17e985df
Austin Baker <noise.9no@gmail.com>
2023-03-09 21:04:20 -0500

    Base commit

---
+++ src/services/algorithm.ts
@@ -0,0 +1,34 @@
+interface TeamProbabilityDecimalResult {
+  blueProbabilityDecimal: number;
+  orangeProbabilityDecimal: number;
+}
+
+export function calculateProbabilityDecimal(teams: ActiveMatchTeams): TeamProbabilityDecimalResult {
+  const blueTeamMMR = teams.blueTeam.reduce((totalMMR, ballChaser) => totalMMR + ballChaser.mmr, 0);
+  const orangeTeamMMR = teams.orangeTeam.reduce((totalMMR, ballChaser) => totalMMR + ballChaser.mmr, 0);
+
+  const calcTeamProbabilityDecimal = (winnerMMR: number, loserMMR: number): number => {
+    const difference = (loserMMR - winnerMMR) / 400;
+    const power = Math.pow(10, difference) + 1;
+    const probabilityDecimal = 1 / power;
+    return probabilityDecimal;
+  };
+
+  return {
+    blueProbabilityDecimal: calcTeamProbabilityDecimal(blueTeamMMR, orangeTeamMMR),
+    orangeProbabilityDecimal: calcTeamProbabilityDecimal(orangeTeamMMR, blueTeamMMR),
+  };
+}
+
+export function calculateMMR(calculatedProbabilityDecimal: number): number {
+  let mmr = (1 - calculatedProbabilityDecimal) * 20;
+  mmr = Math.min(15, mmr);
+  mmr = Math.max(5, mmr);
+
+  return Math.round(mmr);
+}
+
+export function calculateProbability(calculatedProbabilityDecimal: number): number {
+  const probability = calculatedProbabilityDecimal * 100;
+  return Math.round(probability);
+}
