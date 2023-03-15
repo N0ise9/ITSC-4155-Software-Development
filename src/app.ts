27f3c7e55106ab158695bf682424be086833a1b4
JacobHarland <geicojacob@gmail.com>
2023-03-15 14:46:04 -0400

    restoring accidental file deletion

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import path from "node:path";
 import routes from "./routes/testRoutes";
 
 const app: Application = express();
+/* eslint-disable no-console */
+export function test() {
+  return "test";
+}
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-console */
+export function test() {
+  return "test";
+}
