3ed7b4461a4dc6583d0dea1885f451af7bd64594
JacobHarland <geicojacob@gmail.com>
2023-03-16 15:43:28 -0400

    refactor

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -4,6 +4,22 @@ export const index = (req: Request, res: Response) => {
   res.render("index");
 };
 
-export const test = (req: Request, res: Response) => {
-  res.render("test");
+export const profile = (req: Request, res: Response) => {
+  res.render("profile");
+};
+
+export const signin = (req: Request, res: Response) => {
+  res.render("signin");
+};
+
+export const signuplogin = (req: Request, res: Response) => {
+  res.render("signuplogin");
+};
+
+export const survey = (req: Request, res: Response) => {
+  res.render("survey");
+};
+
+export const thankyou = (req: Request, res: Response) => {
+  res.render("thankyou");
 };
--- src/routes/testRoutes.ts
+++ src/routes/testRoutes.ts
@@ -3,6 +3,10 @@ import * as controller from "../controllers/testController";
 
 const router = express.Router();
 router.get("/", controller.index);
-router.get("/test", controller.test);
+router.get("/profile", controller.profile);
+router.get("/signin", controller.signin);
+router.get("/signuplogin", controller.signuplogin);
+router.get("/survey", controller.survey);
+router.get("/thankyou", controller.thankyou);
 
 export = router;
