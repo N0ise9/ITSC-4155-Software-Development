1e42bd2c9add37416bd42e5bb32e8122a430abb2
Austin Baker <noise.9no@gmail.com>
2023-03-21 15:36:28 -0400

    Changed getFood() to checkIfFoodMatch(): boolean Added getters

--- src/repositories/algorithmRepository.ts
+++ src/repositories/algorithmRepository.ts
@@ -1,37 +1,53 @@
 import { UserInSite, FoodFondness, FoodRankings, Food } from "../utils/types";
 
-interface MatchingFood {
-  userFood: string;
-  fondnessFood: string;
-  rankingFood: string;
-}
-export function getFood(
+export async function checkIfFoodMatch(
   user: UserInSite,
   food: Food,
   foodFondness: FoodFondness,
   foodRanking: FoodRankings
-): MatchingFood | undefined {
+): Promise<boolean> {
   if (!user.foodChoices) {
     console.warn("User has not selected any food.");
-    return;
+    return false;
   }
   const usersChoice = user.foodChoices.find((userChoice) => userChoice === food);
   const fondnessMatch = user.foodFondness.find((userChoice) => userChoice === foodFondness);
   const rankingMatch = user.foodRankings.find((userChoice) => userChoice === foodRanking);
   if (usersChoice === undefined) {
     console.warn("No matching user food choice.");
+    return false;
   } else if (fondnessMatch === undefined) {
     console.warn("No matching user food fondness.");
+    return false;
   } else if (rankingMatch === undefined) {
     console.warn("No matching user food ranking.");
+    return false;
+  } else if (user.id !== foodFondness.user && user.id !== foodRanking.user) {
+    console.warn("The user does not match the given food.");
+    return false;
+  } else if (foodFondness.id !== foodRanking.id) {
+    console.warn("The foodFondness and foodRanking does not match.");
+    return false;
   } else {
-    const userFood = usersChoice.id;
-    const fondnessFood = fondnessMatch.id;
-    const rankingFood = rankingMatch.id;
-    return {
-      fondnessFood,
-      rankingFood,
-      userFood,
-    };
+    user.foodChoices?.forEach((userFood) => {
+      if (userFood.id !== foodFondness.id && userFood.id !== foodRanking.id) {
+        console.warn("User food choices do not match FoodFondness or FoodRanking type");
+        return false;
+      }
+    });
+    return true;
+  }
+}
+
+export function getRanking(foodRankings: FoodRankings): number {
+  return foodRankings.rank;
+}
+
+export function getFondness(foodFondness: FoodFondness): number | undefined {
+  if (foodFondness.fondness === undefined) {
+    console.warn("The user has not selected a fondness level for this food.");
+    return;
+  } else {
+    return foodFondness.fondness;
   }
 }
