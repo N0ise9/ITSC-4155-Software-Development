7876555a2f3b9ab6cf9f7f9395a3d37097ce6533
Ramsey <razure@uncc.edu>
2023-03-30 20:26:26 -0400

    doubleq

--- src/models/registration.js
+++ src/models/registration.js
@@ -1,16 +1,16 @@
 const bcrypt = require('bcrypt');
 const userSchema = new Schema({
-    firstName: {type: String, required: [true, 'first name is required']},
-    lastName: {type: String, required: [true, 'last name is required']},
-    email: { type: String, required: [true, 'email address is required'], 
-             unique: [true, 'this email address has been used'] },
-    password: { type: String, required: [true, 'password is required'] },
+    firstName: {type: String, required: [true, "first name is required"]},
+    lastName: {type: String, required: [true, "last name is required"]},
+    email: { type: String, required: [true, "email address is required"], 
+             unique: [true, "this email address has been used"] },
+    password: { type: String, required: [true, "password is required"] },
 }
 );
 
 userSchema.pre('save', function(next){
     let user = this;
-    if (!user.isModified('password'))
+    if (!user.isModified("password"))
         return next();
     bcrypt.hash(user.password, 4)
     .then(hash => {
@@ -28,4 +28,4 @@ userSchema.pre('save', function(next){
 
 
 
-module.exports = PostgreSQL.model('user', userSchema);
+module.exports = PostgreSQL.model("user", userSchema);
--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -4,21 +4,21 @@ import * as controller from "../controllers/registration";
 const router = express.Router();
 
 //GET /users/new: send html form for creating a new user account
-router.get('/registration', controller.new);
+router.get("/registration", controller.registration);
 
 //POST /users: create a new user account
-router.post('/', controller.create);
+router.post("/", controller.create);
 
 //GET /users/login: send html for logging in
-router.get('/login',  controller.getUserLogin);
+router.get("/login",  controller.getUserLogin);
 
 //POST /users/login: authenticate user's login
-router.post('/login',  controller.login);
+router.post("/login",  controller.login);
 
 //GET /users/profile: send user's profile page
-router.get('/profile', controller.profile);
+router.get("/profile", controller.profile);
 
 //POST /users/logout: logout a user
-router.get('/logout', controller.logout);
+router.get("/logout", controller.logout);
 
 export = router;
