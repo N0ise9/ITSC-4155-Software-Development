409cdea66f731c4bb6fa3cf34b7fbe7519ed37fa
JacobHarland <geicojacob@gmail.com>
2023-04-05 16:50:08 -0400

    function to remove unwanted values in yelp object

---
+++ src/services/yelp/cleanYelp.ts
@@ -0,0 +1,35 @@
+import * as fs from "fs";
+
+fs.readFile("./src/services/yelp/outputYelp.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  const dataToJson = JSON.parse(data);
+  const newArray = dataToJson.map(
+    (object: {
+      id: string;
+      name: string;
+      image_url: string;
+      url: string;
+      review_count: number;
+      categories: string[];
+      rating: number;
+      price: string;
+      location: string[];
+      phone: string;
+      display_phone: string;
+    }) => {
+      const { id, name, image_url, url, review_count, categories, rating, price, location, phone, display_phone } =
+        object;
+      // eslint-disable-next-line sort-keys
+      return { id, name, image_url, url, review_count, categories, rating, price, location, phone, display_phone };
+    }
+  );
+
+  // TODO: data.json is for testing, change to outputYelp.json
+  fs.appendFile("./data.json", JSON.stringify(newArray, null, 2), (err: any) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
