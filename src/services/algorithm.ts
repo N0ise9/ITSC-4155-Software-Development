a172f01247906c1a7bfc69909ad79a83c2835e32
Austin Baker <noise.9no@gmail.com>
2023-04-24 11:26:10 -0400

    added flavor to algorithm

--- src/services/algorithm.ts
+++ src/services/algorithm.ts
@@ -24,3 +24,26 @@ export function calculateCuisineMatches(user: UserInSite): number[] {
   }
   return mmrArray;
 }
+
+export function calculateFlavorMatches(user: UserInSite): number[] {
+  const calcProbabilityDecimal = (current: number, previous: number): number => {
+    const difference = (previous - current) / 400;
+    const power = Math.pow(10, difference) + 1;
+    const probabilityDecimal = 1 / power;
+    // console.log("\n\nDifference: " + difference + "\nPower: " + power + "\nProbDecimal:" + probabilityDecimal);
+    return probabilityDecimal * 2;
+  };
+  const mmrArray = [];
+  const flavorList = user.flavorMMR;
+
+  const orderedList = flavorList.sort((a, b) => b.mmr - a.mmr);
+  const first = orderedList[0];
+  for (let i = 0; i < orderedList.length; i++) {
+    if (i == 0) {
+      mmrArray[i] = calcProbabilityDecimal(first.mmr, first.mmr);
+    } else {
+      mmrArray[i] = calcProbabilityDecimal(first.mmr, orderedList[i].mmr);
+    }
+  }
+  return mmrArray;
+}
