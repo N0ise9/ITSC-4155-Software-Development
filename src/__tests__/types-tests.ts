b4a779b0ced37513dbda5ce69c6fbb71075d7f54
Sarnam <spate247@uncc.edu>
2023-04-26 14:56:40 -0400

    wrote test cases for algorithm.ts
    Test Case 1- checks the array length
    Test Case 2- checks the probabilities between 0 and 2

--- src/__tests__/types-tests.ts
+++ src/__tests__/types-tests.ts
@@ -8,16 +8,23 @@ import { FlavorMMR } from "../utils/types";
 import { Food } from "../utils/types";
 import { RestaurantData } from "../utils/types";
 
-const MockFoodMMR = Factory.Sync.makeFactory<FoodMMR>({
+const foodMock = Factory.Sync.makeFactory<FoodMMR>({
     foodID: Factory.each(() => faker.lorem.words(3)), 
     userID: Factory.each(() => faker.lorem.words(3)),
     mmr: faker.datatype.number()
   });
 
-  const MockUserInSite = Factory.Sync.makeFactory<UserInSite>({
+const cuisineMock = Factory.Sync.makeFactory<CuisineMMR>({
+  cuisineID: Factory.each(() => faker.lorem.words(3)),
+  userID: Factory.each(() => faker.lorem.words(3)),
+  mmr: faker.datatype.number()
+});
+
+
+  const userInSiteMock = Factory.Sync.makeFactory<UserInSite>({
     id: Factory.each(() => faker.lorem.words(3)),
-    foodMMR: faker.datatype.array()
-    cuisineMMR: Array<CuisineMMR>;
-    flavorMMR: Array<FlavorMMR>;
-    userRanking: Array<UserFoodRanking>;
+    foodMMR: faker.foodMMR.array(),
+    cuisineMMR: faker.cuisineMMR.array(),
+    flavorMMR: faker.flavorMMR.array(),
+    userRanking: faker.userRanking.array()
   });
