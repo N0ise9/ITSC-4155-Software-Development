f6afe1e2777e5d91e0d3bf83a8325b06c79ae0ea
cgross15 <cgross15@uncc.edu>
2023-04-25 23:18:34 -0400

    added mocks for food, flavor, & cuisine MMR (#84)

--- src/__tests__/types-tests.ts
+++ src/__tests__/types-tests.ts
@@ -7,6 +7,7 @@ import { CuisineMMR } from "../utils/types";
 import { FlavorMMR } from "../utils/types";
 import { Food } from "../utils/types";
 import { RestaurantData } from "../utils/types";
+import { read } from "fs";
 
 const MockFoodMMR = Factory.Sync.makeFactory<FoodMMR>({
     foodID: Factory.each(() => faker.lorem.words(3)), 
@@ -14,10 +15,22 @@ const MockFoodMMR = Factory.Sync.makeFactory<FoodMMR>({
     mmr: faker.datatype.number()
   });
 
-  const MockUserInSite = Factory.Sync.makeFactory<UserInSite>({
+  const MockFlavorMMR = Factory.Sync.makeFactory<FlavorMMR>({
+    flavorID: Factory.each(() => faker.lorem.words(3)), 
+    userID: Factory.each(() => faker.lorem.words(3)),
+    mmr: faker.datatype.number()
+  });
+
+  const MockCuisineMMR = Factory.Sync.makeFactory<CuisineMMR>({
+    cuisineID: Factory.each(() => faker.lorem.words(3)), 
+    userID: Factory.each(() => faker.lorem.words(3)),
+    mmr: faker.datatype.number()
+  });
+
+  /*const MockUserInSite = Factory.Sync.makeFactory<UserInSite>({
     id: Factory.each(() => faker.lorem.words(3)),
-    foodMMR: faker.datatype.array()
+    foodMMR: faker.helpers.arrayElement<FoodMMR>(array: readonly MockFoodMMR[] = []): MockFoodMMR, 
     cuisineMMR: Array<CuisineMMR>;
     flavorMMR: Array<FlavorMMR>;
     userRanking: Array<UserFoodRanking>;
-  });
+  });*/ 
