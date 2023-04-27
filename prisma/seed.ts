3cc4af5325364e77d7360b03fcf244a32279e9d3
JacobHarland <geicojacob@gmail.com>
2023-04-27 18:16:51 -0400

    merged algorithm branch

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,22 +4,13 @@ import * as fs from "fs";
 
 const prisma = new PrismaClient();
 
-const restaurants = fs.readFileSync("./src/services/yelp/cleanedYelp.json", "utf-8");
 const foods = fs.readFileSync("./src/services/foodsCollection/modifiedFoods.json", "utf-8");
-const restaurantsPrettyJson = JSON.parse(restaurants);
 const foodsPrettyJson = JSON.parse(foods);
 
 async function main() {
-  for (let restaurant of restaurantsPrettyJson) {
-    console.info(restaurant);
-    await prisma.restaurant.create({
-      data: restaurant,
-    });
-  }
-
   for (let food of foodsPrettyJson) {
     console.info(food);
-    await prisma.restaurantFood.create({
+    await prisma.food.create({
       data: food,
     });
   }
